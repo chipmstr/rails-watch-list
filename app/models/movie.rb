eac19420ee64e474517b85be60df7ec77b7e80f9
chipmstr <dan.ursu@hotmail.com>
2024-10-05 04:11:54 +0200

    rails-watch-list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,7 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   # remove rating validation, as we now handle ratings through reviews
+
+  # Attach an image to the movie
+  has_one_attached :image
 end
