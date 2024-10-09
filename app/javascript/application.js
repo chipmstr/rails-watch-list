90a63979783bb653a84a5be045e8395229689367
chipmstr <dan.ursu@hotmail.com>
2024-10-09 02:59:57 +0200

    Update delete button to use turbo_method

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
-import "@hotwired/turbo-rails";
-import Rails from "@rails/ujs";
-Rails.start();
-
-import "controllers";
-import "bootstrap";
-import "@popperjs/core";
+import "@hotwired/turbo-rails"
+import "controllers"
+import "bootstrap"
+import "@popperjs/core"
